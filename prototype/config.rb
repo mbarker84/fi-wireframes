b6857b7947ddaf49d6a457c13f6f8a148b37db9d
Michelle Barker <mbarker_84@hotmail.co.uk>
2015-02-18 13:49:46 +0000

    Update prototype files

--- prototype/config.rb
+++ prototype/config.rb
@@ -1,6 +1,8 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 require 'susy'
+# https://github.com/chriseppstein/sass-globbing – import multiple Sass files in single statement
+require 'sass-globbing' 
 
 # Set this to the root of your project when deployed:
 http_path = "/"
